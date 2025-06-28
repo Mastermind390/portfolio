2a24e3cf875483a8880917d271332b990b4bbc95
mastermindz390 <enitanelijah39@gmail.com>
2025-06-28 08:59:37 +0100

    make corrections

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "my-portfolio"
-  server : {
-    port : 3000,
-  }
+  base: "/my-portfolio/"
+  // server : {
+  //   port : 3000,
+  // }
 })
